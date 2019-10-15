f9416ec5d10ae7dcf37909fd2e2011e66cb10c9e
Anders Jakobsen <andersjkbsn@gmail.com>
2019-10-15 14:20:24 +0200

    Better dumping

--- dump_imdb.py
+++ dump_imdb.py
@@ -4,7 +4,7 @@ import os
 import threading
 import requests
 import json
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 
 csv_writer_lock = threading.Lock()
 merged = movies.merge(links, on='movieId')
@@ -55,32 +55,55 @@ def handle_movie(movieId, imdbId):
     image_url = get_image_path(soup)
     if image_url:
         save_url_to_file(get_poster(soup), get_image_path(movieId))
+    else:
+        return False
 
     # Save list of actor ids
     actors = get_actors(soup)
     if actors and len(actors) > 0:
         with open(get_actors_path(movieId), 'w') as outFile:
             json.dump(actors, outFile)
+    else:
+        return False
+
+    return True
 
 
 def handle_chunk(chunk):
+    succeeded = 0
     for movieId, imdbId in chunk:
-        handle_movie(movieId, imdbId)
+        if handle_movie(movieId, imdbId):
+            succeeded += 1
+
+    return succeeded
 
 
-if __name__ == "__main__":
+def dump_movies():
     movie_imdb = []
 
     for _, row in merged.iterrows():
         movie_imdb.append((row.movieId, str(row.imdbId).zfill(7)))
 
     print(f'Expected movies: {len(movie_imdb)}')
-    
+
     # Partition into n groups
-    movie_imdb = chunkify(movie_imdb, 20)
+    movie_imdb = chunkify(movie_imdb, 50)
 
-    executor = ThreadPoolExecutor(max_workers=5)
+    executor = ThreadPoolExecutor(max_workers=15)
+    futures = []
     for chunk in movie_imdb:
-        executor.submit(handle_chunk, chunk)
+        futures.append(executor.submit(handle_chunk, chunk))
 
-    executor.shutdown(wait=True)
+    sum_succeeded = 0
+    for future in futures:
+        sum_succeeded += future.result()
+    
+    print(f'Sum succeeded: {sum_succeeded}')
+
+
+def dump_actors():
+    pass
+
+
+if __name__ == "__main__":
+    dump_movies()
