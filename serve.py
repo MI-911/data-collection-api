8908eebef341ab3d78000875046811df6688f428
Theis <jendal@live.dk>
2019-10-18 16:30:29 +0200

    Working threading

--- serve.py
+++ serve.py
@@ -1,9 +1,10 @@
-
+from concurrent.futures import ThreadPoolExecutor, wait
 from random import sample, shuffle
 
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from multiprocessing import Pool
+from itertools import product
 
 import dataset
 from neo import get_relevant_neighbors, get_unseen_entities, sample_relevant_neighbours
@@ -13,7 +14,7 @@ app.secret_key = "XD"
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 MAX_QUESTIONS = 50
-MINIMUM_SEED_SIZE = 10
+MINIMUM_SEED_SIZE = 1
 SESSION = {} 
 N_QUESTIONS = 9
 N_ENTITIES = N_QUESTIONS // 3
@@ -64,13 +65,12 @@ def entities():
         return "Done."  # TODO: PageRank over all likes and dislikes
 
     # Find the relevant neighbors (with page rank) from the liked and disliked seeds
-    liked_relevant, disliked_relevant = get_next_entities(json)
+    liked_relevant, disliked_relevant, random_entities = get_next_entities(json, seen_entities)
 
-    random_entities = get_unseen_entities(seen_entities + liked_relevant + disliked_relevant, N_ENTITIES)
-    random_entities_list = [n for n in random_entities]
+    random_entities = [e for e in random_entities if e not in liked_relevant and e not in disliked_relevant][:N_ENTITIES]
 
     # Return them all to obtain user feedback
-    requested_entities = liked_relevant + disliked_relevant + random_entities_list
+    requested_entities = liked_relevant + disliked_relevant + random_entities
     shuffle(requested_entities)
 
     print(len(requested_entities))
@@ -83,16 +83,22 @@ def main():
     return 'test'
 
 
-def get_next_entities(json): 
-    with Pool(5) as p: 
-        return p.map(get_related_entities, [list(json[LIKED]), list(json[DISLIKED])])
+def get_next_entities(json, seen):
+    f = []
+    with ThreadPoolExecutor(max_workers=3) as e:
+        f.append(e.submit(get_related_entities, list(json['liked']), seen))
+        f.append(e.submit(get_related_entities, list(json['disliked']), seen))
+        f.append(e.submit(get_unseen_entities, seen, N_QUESTIONS))
+
+    wait(f)
+
+    return [element.result() for element in f]
 
 
 def get_related_entities(entities, seen_entities): 
     liked_relevant = get_relevant_neighbors(entities, seen_entities)
     liked_relevant_list = sample_relevant_neighbours(liked_relevant, n_actors=N_ENTITIES // 3, n_directors=N_ENTITIES // 3, n_subjects=N_ENTITIES // 3)
     return liked_relevant_list
-
 
 
 def update_session(liked, disliked, unknown):
